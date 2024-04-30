1cc4574d3b2cae9b899f2f55772b0b6eea89895a
shakeel-ahmed14 <143477606+shakeel-ahmed14@users.noreply.github.com>
2024-04-30 15:05:04 +0530

    Update generate_qr_code.py

--- generate_qr_code.py
+++ generate_qr_code.py
@@ -15,7 +15,7 @@ def generate_qr_code(data, filename):
 
 if __name__ == "__main__":
     # Example data for QR code
-    data = "Gurmeet Singh"
+    data = "I love chai"
 
     # Generate QR code image
     generate_qr_code(data, "qrcode.png")
